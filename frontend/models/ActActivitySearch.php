830b145b1adfdf9da64fae7ad645b1e85b017b1c
Drummerboy458 <1664032805@qq.com>
2019-07-05 12:36:54 +0800

    紫色条修改为更窄

---
+++ frontend/models/ActActivitySearch.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\ActActivity;
+
+/**
+ * ActActivitySearch represents the model behind the search form about `frontend\models\ActActivity`.
+ */
+class ActActivitySearch extends ActActivity
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['title', 'content', 'published_at', 'holded_at', 'updated_at', 'sponsor', 'location'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = ActActivity::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'published_at' => $this->published_at,
+            'holded_at' => $this->holded_at,
+            'updated_at' => $this->updated_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'content', $this->content])
+            ->andFilterWhere(['like', 'sponsor', $this->sponsor])
+            ->andFilterWhere(['like', 'location', $this->location]);
+
+        return $dataProvider;
+    }
+}

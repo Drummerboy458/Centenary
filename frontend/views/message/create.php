a56669828ef123a5a71fcd4dc4fa30b509733d10
lzyhha <819814373@qq.com>
2019-07-05 22:17:01 +0800

    留言页面修改

--- frontend/views/message/create.php
+++ frontend/views/message/create.php
@@ -11,6 +11,7 @@ $this->title = '100|为母校送祝福';
     <div class="col-md-6">
         <div class="message-create">
 
+            <br><br>
             <h2>为母校送祝福</h2>
             <br>
             <?= $this->render('_form', [

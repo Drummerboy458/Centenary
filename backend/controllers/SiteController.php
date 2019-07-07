8db842b603ae014f5d18fc735fc98918fe7133e1
pichunying <2339348937@qq.com>
2019-07-07 12:25:22 +0800

    jump to chart.php

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -26,7 +26,7 @@ class SiteController extends Controller
                         'allow' => true,
                     ],
                     [
-                        'actions' => ['logout', 'index'],
+                        'actions' => ['logout', 'index','chart'],
                         'allow' => true,
                         'roles' => ['?'],
                     ],
@@ -94,5 +94,10 @@ class SiteController extends Controller
         Yii::$app->user->logout();
 
         return $this->goHome();
+    }
+
+    public function actionChart()
+    {
+        return $this->render('chart');
     }
 }
